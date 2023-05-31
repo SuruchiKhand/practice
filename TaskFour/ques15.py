051b14fcdaf940ad3634320f45862d851daf31ee
Suruchi Khand <khand.suruchi@gmail.com>
2023-05-30 20:43:05 -0400

    revised solution

--- TaskFour/ques15.py
+++ TaskFour/ques15.py
@@ -1,11 +1,17 @@
 # 15. Write a program in Python to multiply the elements of a list by itself using a traditional function and pass the function to map() to complete the operation.
 
-def multiply(x):
-    return(x*x)
+# def multiply_elements(list1):
+#     new_list = []
+#     for i in list1:
+#         new_list.append(i * i)
+#     print(new_list)
+    
+def multiply_elements(n):
+    return n * n
 
-func = [multiply]
-for i in range(5):
-    values = list(map(lambda x: x(i), func))
-    print(values)
+def multiply_list(list1):
+    result = map(multiply_elements, list1)
+    return list(result)
 
-
+if __name__ == "__main__":
+    print(multiply_list([1,2,3,4]))
