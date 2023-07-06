52f75cf9312d4c953a73f34141ef1b03a4d53e4d
Suruchi Khand <khand.suruchi@gmail.com>
2023-07-05 22:45:04 -0400

    Revert "remove element solution"
    This reverts commit 4aaccc8c9969184321055eaf7cf0f5289a63f05e.

--- leetcode/extra_prob/ques1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def solution(year):
-    # return math.ceil(year/100)
-    return math.floor(year/100)
-
-if __name__ == "__main__":
-    print(solution(1905))
-    print(solution(99))
